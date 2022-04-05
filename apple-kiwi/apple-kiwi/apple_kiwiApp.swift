39743f1f231cccca808ee03ea2a50a1061d9b2fd
MBSoo <qud9783@khu.ac.kr>
2022-04-05 17:30:21 +0900

    project

---
+++ apple-kiwi/apple-kiwi/apple_kiwiApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_kiwiApp.swift
+//  apple-kiwi
+//
+//  Created by MBSoo on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct apple_kiwiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
